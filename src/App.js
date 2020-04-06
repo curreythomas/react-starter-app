984b8ae6f585801839560927aa19a90f8c098894
Currey Thomas <currey.thomas@gmail.com>
2020-04-05 21:18:51 -0400

    updated sentence grammar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
             <p>To start the server, first run <code> > npm install</code> followed by <code> > npm run build</code>.</p> 
             <p>This will install all dependencies and build the project creating a new folder <code> dist</code> with an index.html file and bundle.js file.</p>
             <p>The last step to start the server is to run <code> > npm run start</code>. This will open the application in your browser at <a href="localhost:8080/" alt="link to localhost:8080">localhost:8080/</a></p>
-            <p>To add components, add a .js file to the components folder, and import the component into App.js. If you have any assets, place those in the assets folder.</p>
+            <p>To add components, create a .js file and add it to the components folder. Import the component into App.js. If you have any assets, place those in the assets folder.</p>
             <img src={ReactWebpackBabel} alt="react webpack babel logos" width='100%' />
         </div>
     )
